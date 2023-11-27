8249f274396742b0b71f94a8e5d3fe5c79e12ceb
Vinicius Rodrigues Rosa <vinicius.r.rosa@accenture.com>
2023-11-27 20:53:01 -0300

    FIX: Change variable name

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost:4000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
 const api = axios.create({
   baseURL: API_URL
